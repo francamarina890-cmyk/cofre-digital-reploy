0f18be20086d2bfa051c92b2ab0b7e7c8d1dd119
francamarina890-cmyk <francamarina890@gmail.com>
2026-05-14 10:20:06 -0300

    Create security_utils.py

---
+++ security_utils.py
@@ -0,0 +1,46 @@
+import re
+import logging
+from functools import wraps
+class SecureLogger:
+def __init__(self, logger_name):
+self.logger = logging.getLogger(logger_name)
+self.sensitive_patterns = [
+(r&#39;password[&quot;\s]*[:=][&quot;\s]*([^&quot;\s,}]+)&#39;, r&#39;password=&quot;***MASKED***&quot;&#39;),
+(r&#39;api[_-]?key[&quot;\s]*[:=][&quot;\s]*([^&quot;\s,}]+)&#39;, r&#39;api_key=&quot;***MASKED***&quot;&#39;),
+(r&#39;token[&quot;\s]*[:=][&quot;\s]*([^&quot;\s,}]+)&#39;, r&#39;token=&quot;***MASKED***&quot;&#39;),
+(r&#39;secret[&quot;\s]*[:=][&quot;\s]*([^&quot;\s,}]+)&#39;, r&#39;secret=&quot;***MASKED***&quot;&#39;)
+]
+def _mask_sensitive_data(self, message):
+for pattern, replacement in self.sensitive_patterns:
+message = re.sub(pattern, replacement, message, flags=re.IGNORECASE)
+return message
+
+[SIS] [U3] [C5]
+
+5 / 17
+
+def info(self, message):
+self.logger.info(self._mask_sensitive_data(str(message)))
+def error(self, message):
+self.logger.error(self._mask_sensitive_data(str(message)))
+def mask_secret(secret, visible_chars=4):
+if not secret or len(secret) &lt;= visible_chars * 2:
+return &quot;***MASKED***&quot;
+return secret[:visible_chars] + &quot;*&quot; * (len(secret) - visible_chars * 2) + secret[-
+visible_chars:]
+def secure_log_decorator(func):
+@wraps(func)
+def wrapper(*args, **kwargs):
+secure_logger = SecureLogger(func.__module__)
+safe_kwargs = {k: (mask_secret(str(v)) if any(x in k.lower() for x in
+[&#39;password&#39;, &#39;key&#39;, &#39;token&#39;, &#39;secret&#39;]) else v) for k, v in kwargs.items()}
+secure_logger.info(f&quot;Executando {func.__name__} com args:
+{safe_kwargs}&quot;)
+try:
+result = func(*args, **kwargs)
+secure_logger.info(f&quot;{func.__name__} executado com sucesso&quot;)
+return result
+except Exception as e:
+secure_logger.error(f&quot;Erro em {func.__name__}: {str(e)}&quot;)
+raise
+return wrapper
